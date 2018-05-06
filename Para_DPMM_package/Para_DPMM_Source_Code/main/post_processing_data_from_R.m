987a67e6cf0ff9f8a4464085ce78c248bdbbcb38
tiehangd <35096848+tiehangd@users.noreply.github.com>
2018-05-06 15:55:00 -0700

    source code

---
+++ Para_DPMM_package/Para_DPMM_Source_Code/main/post_processing_data_from_R.m
@@ -0,0 +1,13 @@
+
+clear all;
+load('R_matrix_to_pcareducemat.mat'); % edit the file name based on the output from the interface programs to comparison methods.
+
+z_pcareduce=z_pcareduce+1;
+
+[AR,RI,MI,HI]=RandIndex(z_pcareduce,label_new);
+
+
+
+
+
+
