21bd3d1dad9f149f671f73edf0b1cafc4782f82e
Tiehang Duan <35096848+tiehangd@users.noreply.github.com>
2018-06-26 15:04:36 -0700

    Update sequencing_depth_controller.m

--- Para_DPMM_package/sequencing_depth_control/sequencing_depth_controller.m
+++ Para_DPMM_package/sequencing_depth_control/sequencing_depth_controller.m
@@ -2,11 +2,12 @@
 load('data_matrix_1_S_Set.mat');
 [gene_num,cell_num]=size(full_gene_trun_cell_trun_comb2_perm);
 new_matrix=zeros(gene_num,cell_num);
+seq_depth_ratio=0.875
 
 for i =1:cell_num
     this_col=full_gene_trun_cell_trun_comb2_perm(:,i);
     total_count=sum(this_col);
-    sample_size=floor(total_count*7/8);
+    sample_size=floor(total_count*seq_depth_ratio);
     dis_prob=this_col/total_count;
     x = discretesample(dis_prob, sample_size);
     new_matrix(:,i)=countmember(1:gene_num,x);
@@ -14,4 +15,4 @@ end
 
 full_gene_trun_cell_trun_comb2_perm=new_matrix;
 
-save('../Multinomial/S_Set_seq_dep_2100.mat','full_gene_trun_cell_trun_comb2_perm','full_gene_trun_cell_trun_label_comb2_perm');
+save('../Multinomial/S_Set_seq_dep.mat','full_gene_trun_cell_trun_comb2_perm','full_gene_trun_cell_trun_label_comb2_perm');
